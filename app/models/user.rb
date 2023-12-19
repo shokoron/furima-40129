f5c5940346c4e7a3f3c6b84481b5e12ca0d77eac
shokoron <shokoron2011@gmail.com>
2023-12-19 20:40:34 +0900

    修正しました
    修正したテスト画面
    https://i.gyazo.com/4a788e4defd841f0f57895ff3c607624.png

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角文字を使用してください' }
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角文字を使用してください' }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶ一-]+\z/, message: 'は全角カタカナを使用してください' }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶ一-]+\z/, message: 'は全角カタカナを使用してください' }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カタカナを使用してください' }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カタカナを使用してください' }
   validates :birthday, presence: true
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
