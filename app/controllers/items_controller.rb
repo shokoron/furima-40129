37db82bc4edf9c6ebbd8beafc94beaddff584b08
shokoron <shokoron2011@gmail.com>
2023-12-22 18:03:45 +0900

    商品出品機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,27 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private # rubocop:disable Layout/EmptyLinesAroundAccessModifier
+  def item_params
+    params.require(:item).permit(
+      :name, :description, :category_id, :condition_id,
+      :shipping_charge_id, :shipping_area_id, :shipping_day_id,
+      :price, :image
+    ).merge(user_id: current_user.id)
   end
 end
