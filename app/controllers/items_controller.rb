d1308b1fbf1f7340fceb0f5b4693a950a3c2b0bb
shokoron <shokoron2011@gmail.com>
2023-12-24 15:46:05 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :check_user_authorization, only: [:edit, :update]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -22,6 +24,19 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private # rubocop:disable Layout/EmptyLinesAroundAccessModifier
   def item_params
     params.require(:item).permit(
@@ -29,5 +44,15 @@ class ItemsController < ApplicationController
       :shipping_charge_id, :shipping_area_id, :shipping_day_id,
       :price, :image
     ).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def check_user_authorization
+    unless current_user == @item.user
+      redirect_to root_path
+    end
   end
 end
