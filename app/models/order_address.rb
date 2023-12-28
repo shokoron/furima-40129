9978652432d473fbfd4b9597f2ede8b2e24bf980
shokoron <shokoron2011@gmail.com>
2023-12-28 15:43:40 +0900

    修正しました
    テスト修正版
    https://i.gyazo.com/966e277afe120c59c2a549eba06e88e1.png

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -8,7 +8,7 @@ class OrderAddress
     validates :city
     validates :street_address
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "is invalid. Input only number without hyphen and must be 10 or 11 digits" }
-    validates :postal_code, presence: true, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
     validates :token
   end
   validates :shipping_area_id, numericality: { other_than: 1, message: "can't be blank" }
